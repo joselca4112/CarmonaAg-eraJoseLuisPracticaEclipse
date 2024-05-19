00a0d2ae740ad5e0cb4b54872395a6b973388784
joselca4112 <jlcarmo2002@gmail.com>
2024-05-19 23:38:24 +0200

    subida1

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package unico;
+
+public class HelloWorld
+{
+	//Modificacion
+ // Tu programa comienza con una llamada a main().
+ // Imprime "Hola Mundo" a la ventana de la terminal.
+ public static void main(String args[])
+ {
+ System.out.println("Hola Mundo");
+ }
+}
+
